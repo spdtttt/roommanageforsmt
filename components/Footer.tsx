b0ef812180ceedbbfc907e6fa72e0de2745e535a
mosixmus <spdt101mos@gmail.com>
2025-11-04 19:53:59 +0700

    edit footer line

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="mb-5">
+    <div className="mb-5 mt-2">
         <div className="text-center text-gray-400 text-lg">
             <p>โรงเรียนเมืองสุราษฎร์ธานี</p>
         </div> 
@@ -9,7 +9,8 @@ const Footer = () => {
             width: '85%',
             height: '1.5px',
             backgroundColor: '#c7c7c7',
-            marginTop: '10px',
+            marginTop: '5px',
+            display: 'flex',
             justifySelf: 'center'
         }}>
         </div>
